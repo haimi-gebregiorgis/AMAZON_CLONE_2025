a896a135526046461f9e8bda74b40749566ba8fc
haimi-gebregiorgis <haimanotwolde1@gmail.com>
2025-06-23 21:51:03 -0400

    endpoint chagned to firebase

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -5,8 +5,8 @@ export const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-75af5/us-central1/api",
 
   //deployed firebase function baseURL
-  // baseURL: "https://api-ptzm527cxa-uc.a.run.app",
+  baseURL: "https://api-ptzm527cxa-uc.a.run.app",
  
   //deployed version of amazon server on render.com
-  baseURL: "https://amazon-api-deploy-whuv.onrender.com",
+  // baseURL: "https://amazon-api-deploy-whuv.onrender.com",
 });
